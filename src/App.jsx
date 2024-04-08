543512cbb95342013f3baf741c199b9c6bb2acac
Habib-0007 <adebayohabib7@gmail.com>
2024-04-08 21:00:22 +0100

    made modifications to the component

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Home from "./pages/Home";
 import BackgroundColor from "./pages/BackgroundColor";
 import LinearGradient from "./pages/LinearGradient";
 import RadialGradient from "./pages/RadialGradient";
+import FontSize from "./pages/FontSize";
 
 const App = () => {
 	const router = createBrowserRouter(
@@ -24,17 +25,18 @@ const App = () => {
 					element={<Home />}
 				/>
 				<Route
-					path="Background Color"
+					path="/Background Color"
 					element={<BackgroundColor />}
 				/>
 				<Route
-					path="Linear Gradient"
+					path="/Linear Gradient"
 					element={<LinearGradient />}
 				/>
 				<Route
-					path="Radial Gradient"
+					path="/Radial Gradient"
 					element={<RadialGradient />}
 				/>
+				<Route path="/Font-size" element={<FontSize />} />
 			</>
 		)
 	);
